fcfa8ef58211e9896a6dc422c30c28f3187ac1f0
Shashank Agrawal <shashank@letscooee.com>
2021-09-08 23:25:56 +0530

    refactor: Use abbreviations in storage key names to reduce readability in production

--- src/constants.ts
+++ src/constants.ts
@@ -11,14 +11,13 @@ export class Constants {
     static LOG_PREFIX: string = 'CooeeSDK';
     static UUID: string = 'uuid';
 
-    static STORAGE_SDK_TOKEN: string = 'sdk_token';
-    static STORAGE_USER_ID: string = 'user_id';
-    static STORAGE_SESSION_NUMBER: string = 'session_number';
-    static STORAGE_FIRST_TIME_LAUNCH: string = 'is_first_launch';
-    static STORAGE_SESSION_START_TIME: string = 'session_start_time';
-    static STORAGE_SESSION_ID: string = 'session_id';
-    static STORAGE_LAST_TOKEN_ATTEMPT: string = 'last_token_check_attempt';
-    static STORAGE_LAST_ACTIVE: string = 'last_active';
+    static STORAGE_SDK_TOKEN: string = 'st';
+    static STORAGE_USER_ID: string = 'uid';
+    static STORAGE_SESSION_NUMBER: string = 'sn';
+    static STORAGE_FIRST_TIME_LAUNCH: string = 'ifl';
+    static STORAGE_SESSION_START_TIME: string = 'sst';
+    static STORAGE_SESSION_ID: string = 'sid';
+    static STORAGE_LAST_ACTIVE: string = 'la';
 
     static IDLE_TIME_IN_SECONDS: number = 30 * 60;
 
