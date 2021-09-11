df1ebba1f774d34576b18025653db3b490e8e757
Shashank Agrawal <shashank@letscooee.com>
2021-09-11 23:41:12 +0530

    chore: Add test code for react app.

--- apps/react-app/src/App.js
+++ apps/react-app/src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import CooeeSDK from "@letscooee/web-sdk";
 
 function App() {
+    CooeeSDK.init("60ace21f18d1ec7c04d12d68", "abXgJzVEXqdPMqRdv7rP");
+    CooeeSDK.setDebug(true);
+    CooeeSDK.setWebAppVersion("0.2.4+204")
+    CooeeSDK.sendEvent("Try React", {foo: "bar"})
+
     return (
         <div className="App">
             <header className="App-header">
