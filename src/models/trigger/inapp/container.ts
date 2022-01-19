9c068fa91dd99659bfc9e71de8e2c51a8fc08290
Shashank Agrawal <shashank@letscooee.com>
2022-01-19 12:27:05 +0530

    fix: Coordinate for East aligned engagements

--- src/models/trigger/inapp/container.ts
+++ src/models/trigger/inapp/container.ts
@@ -30,7 +30,7 @@ export class Container extends BaseElement {
         } else if (this.o === ContainerOrigin.E) {
             styles = {
                 top: '50%',
-                left: 0,
+                right: 0,
                 transform: 'translateY(-50%)',
             };
         } else if (this.o === ContainerOrigin.SE) {
