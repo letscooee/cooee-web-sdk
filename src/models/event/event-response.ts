7be4213e974155631d40d86098009d2502f77a27
abhishek <abhishek@letscooee.com>
2022-11-16 13:42:34 +0530

    refactor 📦: Extends DeviceAuthResponse to EventResponse COOEE-1067

--- src/models/event/event-response.ts
+++ src/models/event/event-response.ts
@@ -1,4 +1,5 @@
 import {TriggerData} from '../trigger/trigger-data';
+import {DeviceAuthResponse} from '../auth/device-auth-response';
 
 /**
  * This is an interface to store event response from server.
@@ -6,13 +7,10 @@ import {TriggerData} from '../trigger/trigger-data';
  * @author Abhishek Taparia
  * @version 0.0.5
  */
-export interface EventResponse {
+export interface EventResponse extends DeviceAuthResponse {
 
     triggerData: TriggerData | undefined;
     eventID: string | undefined;
     sessionID: string | undefined;
-
-    userID: string;
-    sdkToken: string;
 
 }
