c8cd9905dbe4c8159d1da5645d3a424a031ad698
Ashish Gaikwad <ashish@letscooee.com>
2022-11-29 18:02:24 +0530

    refactor: Introduce and use shadow X & Y position

--- src/models/trigger/blocks/shadow.ts
+++ src/models/trigger/blocks/shadow.ts
@@ -5,16 +5,21 @@ export class Shadow {
     readonly blur: number;
     readonly spr: number;
     readonly clr: Color;
+    readonly x: number;
+    readonly y: number;
 
     constructor(data: Partial<Shadow>) {
         data = data ?? {};
         this.blur = data.blur as number;
         this.spr = data.spr as number;
+        this.x = data.x as number;
+        this.y = data.y as number;
         this.clr = new Color(data.clr as Record<string, any>);
     }
 
     getStyle(scalingFactor: number): string {
-        return `0px 0px ${this.blur * scalingFactor}px ${this.spr * scalingFactor}px ${this.clr.rgba}`;
+        return `${this.x * scalingFactor}px ${this.y * scalingFactor}px ` +
+            `${this.blur * scalingFactor}px ${this.spr * scalingFactor}px ${this.clr.rgba}`;
     }
 
 }
