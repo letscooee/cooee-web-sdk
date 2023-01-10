fa8b6a7e1dbf6b4e518c7485bd41518af4178b4c
Shashank Agrawal <shashank@letscooee.com>
2023-01-10 14:13:05 +0530

    chore: Log about Cooee in console

--- src/init/bootstrap.ts
+++ src/init/bootstrap.ts
@@ -15,12 +15,35 @@ export class Bootstrap {
      * Initialize the document/window level workers.
      */
     init(): void {
+        this.logBrand();
         new VisibilityListener().listen();
         // @ts-ignore
         if (!window['disableLegacyCooeeScreenView']) {
             new CommonListeners().listen();
         }
         new ObjectMeddler().meddle();
+    }
+
+    private logBrand(): void {
+        const styles = `
+            display: inline-block;
+            font-size: 14px;
+            background: linear-gradient(90deg, #d52dc8 25%, #2179e2 82%);
+            color: white;
+            padding: 4px;
+            border-radius: 4px;
+        `;
+
+        let text = '\n';
+        text += '📣 Engage every customer uniquely\n';
+        text += '📈 Incremental increase in online sales\n';
+        text += '🤖 AI-powered creative automation via a Canva style editor\n';
+        text += '\n\n';
+        text += 'Learn more: https://www.letscooee.com\n';
+
+        console.group('%c 1-to-1 personalised notifications powered by Cooee ', styles);
+        console.log(`%c${text}`, 'font-size: 13px; line-height: 1.8;');
+        console.groupEnd();
     }
 
 }
