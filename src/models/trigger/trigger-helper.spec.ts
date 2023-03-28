0cf6eba22c6aa2582388bddb57d9c522f835008b
Ashish Gaikwad <ashish@letscooee.com>
2023-03-28 20:05:12 +0530

    test: Update test cases for trigger-helper COOEE-1049

--- src/models/trigger/trigger-helper.spec.ts
+++ src/models/trigger/trigger-helper.spec.ts
@@ -1,4 +1,3 @@
-import {Constants} from '../../constants';
 import {DateUtils} from '../../utils/date-utils';
 import {LocalStorageHelper} from '../../utils/local-storage-helper';
 import {TriggerData} from './trigger-data';
@@ -7,12 +6,12 @@ import {TriggerHelper} from './trigger-helper';
 describe('TriggerHelper', () => {
     describe('storeActiveTrigger', () => {
         it('should do nothing when trigger id is missing', () => {
-            expect(TriggerHelper.storeActiveTrigger(undefined)).toEqual([]);
+            expect(TriggerHelper.storeActiveTrigger(undefined)).toEqual(undefined);
         });
 
         it('should do nothing when trigger id has value "test"', () => {
             const triggerData = new TriggerData({id: 'test'});
-            expect(TriggerHelper.storeActiveTrigger(triggerData)).toEqual([]);
+            expect(TriggerHelper.storeActiveTrigger(triggerData)?.triggerID).toBeUndefined();
         });
 
         it('should save an embedded trigger (and prevent duplicate)', () => {
@@ -24,16 +23,11 @@ describe('TriggerHelper', () => {
 
             TriggerHelper.storeActiveTrigger(triggerData);
             // Store the same value twice
-            const triggers = TriggerHelper.storeActiveTrigger(triggerData);
+            const trigger = TriggerHelper.storeActiveTrigger(triggerData);
 
-            expect(triggers.length).toEqual(1);
-            expect(triggers[0].triggerID).toEqual('12345');
-
-            expect(LocalStorageHelper.setArray).toHaveBeenCalledTimes(2);
-            expect(LocalStorageHelper.setArray).toHaveBeenCalledWith(Constants.STORAGE_ACTIVE_TRIGGERS, triggers);
+            expect(trigger?.triggerID).toEqual('12345');
 
             expect(LocalStorageHelper.setObject).toHaveBeenCalledTimes(2);
-            expect(LocalStorageHelper.setObject).toHaveBeenCalledWith(Constants.STORAGE_ACTIVE_TRIGGER, triggers[0]);
         });
     });
 });
